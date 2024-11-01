230c2ed20c506b81ce3fcc32e843cc626e02d377
Fox Say <Lisick1999@yandex.ru>
2024-11-01 21:08:14 +0300

    js

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const form = {
   email: document.getElementById('email'),
   password: document.getElementById('password'),
@@ -28,42 +27,19 @@ function handleInput(e, name) {
   checkForm();
 };
 
-form.email.oninput = (e) => handleInput(e, 'email');
-form.password.oninput = (e) => handleInput(e, 'password');
-form.button.onclick = () => alert('вы вошли');
-=======
-const form = {
-  email: document.getElementById('email'),
-  password: document.getElementById('password'),
-  button: document.querySelector('.btn'),
-}
-
-function checkForm() {
-  const email = form.email.getElementsByTagName('input')[0].value;
-  const password = form.password.getElementsByTagName('input')[0].value;
-
-  if (email && password) {
-    form.button.classList.remove('disable');
+function validateEmail() {
+  const { value } = form.email.getElementsByTagName('input')[0];
+  const reg = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+  console.log(reg.test(value));
+  if (reg.test(value)) {
+    alert('вы вошли');
   } else {
     form.button.classList.add('disable');
+    form.email.classList.add('error');
   }
 }
 
-function handleInput(e, name) {
-  const { value } = e.target;
-
-  if (value) {
-    form[name].classList.add('filed');  
-  } else {
-    form[name].classList.remove('filed');
-  }
-
-  checkForm();
-};
-
 form.email.oninput = (e) => handleInput(e, 'email');
 form.password.oninput = (e) => handleInput(e, 'password');
-form.button.onclick = () => alert('вы вошли');
+form.button.onclick = () => validateEmail;
 
-//абракадабра
->>>>>>> 390e0bdfda92a692ed2ed14db1811261f1fe1e58
